e087e7fb512c29a23c1044913467bce2edace6da
Steven Senatori <senatori@mail.sfsu.edu>
2013-10-10 01:11:25 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyCrud::Application.config.secret_key_base = '92b39bbf1feb0c5f322e7067e3837b020a74f7d9ebd5a06788fd3e47094e0adf847a9457023bdc0b02cedd165574496ca2dd5d737a8fc94a22d3270635f14cb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyCrud::Application.config.session_store :cookie_store, key: '_ruby_crud_session'
